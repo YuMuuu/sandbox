546e3fa5f3c6147d38b1da6b6e6ebc279e392ffa
YuMuuu <c01150788f@edu.teu.ac.jp>
2018-08-16 17:26:57 +0900

    add dining_philosopher projects
        // loop{
        //
        //    println!("Please input your guess.");
    
        //    let mut guess = String::new();
    
        //    io::stdin().read_line(&mut guess)
        //        .expect("Failed to read line");
    
        //    let guess: u32 = match guess.trim().parse() {
        //        Ok(num) => num,
        //        Err(_) => continue,
        //    };
    
        //     println!("You guessed: {}", guess);
    
        //    match guess.cmp(&secret_number){
        //        Ordering::Less    => println!("Too small!"),
        //        Ordering::Greater => println!("Too big!"),
        //        Ordering::Equal   => {
        //            println!("You win!");
        //            break;
        //        }
        //   Please enter the commit message for your changes. Lines starting

--- sandbox-rust/guessing_game/src/main.rs
+++ sandbox-rust/guessing_game/src/main.rs
@@ -8,32 +8,38 @@ fn main() {
     println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1, 101);
+ 
+    func(secret_number);
+}
 
-    // println!("The secret number is: {}", secret_number);
-
-    loop{
-    
-        println!("Please input your guess.");
+fn guess(secret_number: u32){
+    println!("Please input your guess.");
 
         let mut guess = String::new();
 
         io::stdin().read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
+        //let guess: u32 = match guess.trim().parse() {
+        //    Ok(num) => num,
+        //    Err(_) => func(secret_number),
+        //};
+        let guess: u32 = guess.trim().parse()
+            .expect("Please type a number!");
 
          println!("You guessed: {}", guess);
 
         match guess.cmp(&secret_number){
-            Ordering::Less    => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
+            Ordering::Less    => {
+                println!("Too small!");
+                func(secret_number);
+            }
+            Ordering::Greater => {
+                println!("Too big!");
+                func(secret_number);
+            }
             Ordering::Equal   => {
                 println!("You win!");
-                break;
             }
         }
-    }
 }
